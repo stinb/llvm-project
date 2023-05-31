ef56098eb4c7466f13073ff9e957c043cd5d4c38
Jason Haslam <jason@scitools.com>
2023-05-31 11:27:48 -0600

    Calculate enumerator values for dependent types

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -19719,8 +19719,7 @@ EnumConstantDecl *Sema::CheckEnumConstant(EnumDecl *Enum,
     Val = DefaultLvalueConversion(Val).get();
 
   if (Val) {
-    if (Enum->isDependentType() || Val->isTypeDependent() ||
-        Val->containsErrors())
+    if (Val->isTypeDependent() || Val->containsErrors())
       EltTy = Context.DependentTy;
     else {
       // FIXME: We don't allow folding in C++11 mode for an enum with a fixed
@@ -19794,9 +19793,7 @@ EnumConstantDecl *Sema::CheckEnumConstant(EnumDecl *Enum,
   }
 
   if (!Val) {
-    if (Enum->isDependentType())
-      EltTy = Context.DependentTy;
-    else if (!LastEnumConst) {
+    if (!LastEnumConst) {
       // C++0x [dcl.enum]p5:
       //   If the underlying type is not fixed, the type of each enumerator
       //   is the type of its initializing value:
